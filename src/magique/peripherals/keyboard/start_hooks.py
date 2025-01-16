63ff6cdfcb08972538ac4953f8854c7849d7b5eb
Schtinguerch <schtinguerch@gmail.com>
2025-01-16 21:20:39 +0500

    create: general hook for events of all keys

---
+++ src/magique/peripherals/keyboard/start_hooks.py
@@ -0,0 +1,25 @@
+from typing import List, Any, Literal
+from . import MagiqueKey, OneOfKeys, str_key_dict, Any
+
+from keyboard import hook, KeyboardEvent
+
+
+now_pressed_keys: List[MagiqueKey] = []
+
+
+def on_any_key(event: KeyboardEvent) -> None:
+    key_action: Literal["down", "up"] | None = event.event_type
+
+    if event.name in str_key_dict:
+        key: MagiqueKey = str_key_dict[event.name]
+        str_key_dict[event.name].raise_update_event()
+
+        if key in OneOfKeys.redirect_dict:
+            for dir_key in OneOfKeys.redirect_dict[key]:
+                dir_key.raise_update_event()
+
+    Any.pressed = key_action == "down"
+    Any.raise_update_event()
+
+
+hook(on_any_key)
