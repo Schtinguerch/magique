e4099c2842ff651400fa5007af31b01f139b3f89
Schtinguerch <schtinguerch@gmail.com>
2025-01-24 23:19:08 +0500

    create: Cursor observable fields: x, y, monitor order number

--- src/magique/peripherals/mouse/cursor.py
+++ src/magique/peripherals/mouse/cursor.py
@@ -1,6 +1,6 @@
 from mouse import get_position
 
-from ....magique.declarative import NotifyUpdated
+from ....magique.declarative import NotifyUpdated, notify_property_updated
 
 
 class MagiqueCursor(NotifyUpdated):
@@ -9,10 +9,29 @@ class MagiqueCursor(NotifyUpdated):
         x, y = get_position()
 
         # that fields are updated automatically by general mouse hook
-        self.x: int = x
-        self.y: int = y
-        self.monitor_id: int = 1     # Numeration is based on your PC display setup
-                                     # and numbers given by your OS
-                                     # Default display number in windows, linux and macOS =
+        self._x: int = x
+        self._y: int = y
+        self._monitor_id: int = 1     # Numeration is based on your PC display setup
+                                      # and numbers given by your OS
+                                      # Default display number in windows, linux and macOS =
 
+    @property
+    def x(self) -> int: return self._x
 
+    @x.setter
+    @notify_property_updated(lambda self: self._x)
+    def x(self, new_x: int): self._x = new_x
+
+    @property
+    def y(self) -> int: return self._y
+
+    @y.setter
+    @notify_property_updated(lambda self: self._y)
+    def y(self, new_y: int): self._y = new_y
+
+    @property
+    def monitor_id(self) -> int: return self._monitor_id
+
+    @monitor_id.setter
+    @notify_property_updated(lambda self: self._monitor_id)
+    def monitor_id(self, new_monitor_id: int): self._monitor_id = new_monitor_id
