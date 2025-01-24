e89ccae85f1a09a7cb34cd1bf72a5d02ecd39445
Schtinguerch <schtinguerch@gmail.com>
2025-01-24 23:16:41 +0500

    update: enabled hooks for mouse

--- src/magique/peripherals/mouse/__init__.py
+++ src/magique/peripherals/mouse/__init__.py
@@ -1,2 +1,4 @@
 from .button import MagiqueMouseButton, OneOfMouseButtons, AnyMouseButton
-from .buttons import Left, Middle, Right, Wheel, Any, str_button_dict, all_buttons
+from .buttons import Left, Middle, Right, Wheel, Any, str_button_dict, all_buttons, Cursor, MagiqueCursor
+
+from .start_hooks import now_pressed_buttons
