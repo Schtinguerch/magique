5bfdc7262f733e08395df1510ac3335874b220af
Schtinguerch <schtinguerch@gmail.com>
2025-02-09 18:29:02 +0500

    refactor: applying the pattern method, more easing to children Animation classes implementation

--- src/magique/animations/animation.py
+++ src/magique/animations/animation.py
@@ -1,13 +1,25 @@
+from typing import Any
+from threading import Thread, Lock
+
 from abc import ABC, abstractmethod
 from ..declarative import Observable
 
 
 class Animation(ABC):
+    lock = Lock()
+
     def __init__(
             self,
             target: Observable,
+            from_value: Any = None,
+            to_value: Any = None,
             auto_revert: bool = False,
             repeat_count: int | None = 1):
+
+        self.animation_thread: Thread = self._new_animation_thread()
+
+        self.from_value: Any = from_value or target.value
+        self.to_value: Any = to_value or target.value
 
         self.target: Observable = target
         self.auto_revert: bool = auto_revert
@@ -16,17 +28,93 @@ class Animation(ABC):
         self.stop_signal: bool = False
 
     @abstractmethod
-    def start(self, stop_running_animation: bool = False): pass
+    def _animation_loop(self, is_backward: bool = False): pass
 
-    def pause(self): self.is_active = False
+    def start(self, stop_running_animation: bool = False):
+        """
+        Enables the animation thread. If animation thread is enabled,
+        it can stop it and start new animation thread
 
-    def resume(self): self.is_active = True
+        :param stop_running_animation: If `True` the running thread will be stopped,
+        else the running thread stays running, new animation not starting
+        """
 
-    @abstractmethod
-    def reset(self): pass
+        self.lock.acquire()
+        if self.is_active:
+            if not stop_running_animation:
+                if self.animation_thread.is_alive():
+                    self.lock.release()
+                    return
 
-    @abstractmethod
-    def skip(self): pass
+            self.reset()
 
-    @abstractmethod
-    def wait_until_done(self): pass
+        try:
+            self.is_active = True
+            self.animation_thread.start()
+
+        finally:
+            self.lock.release()
+
+    def pause(self):
+        """
+        Pauses the animation, with ability to resume when it's needed
+        """
+
+        self.is_active = False
+
+    def resume(self):
+        """
+        Resumes the paused animation
+        """
+
+        self.is_active = True
+
+    def reset(self):
+        """
+        Stops the animation thread and sets target value = start animation value (``from_value``)
+        """
+
+        self._stop_animation_thread()
+        self.target.value = self.from_value
+
+    def skip(self):
+        """
+        Stops the animation thread and sets target value = end animation value (``to_value``)
+        """
+
+        self._stop_animation_thread()
+        self.target.value = self.to_value
+
+    def wait_until_done(self):
+        """
+        Joins the animation thread (make you to wait until the animation done)
+        """
+
+        self.animation_thread.join()
+
+    def _stop_animation_thread(self):
+        self.is_active = False
+        self.stop_signal = True
+        self.animation_thread.join()
+        self.animation_thread = self._new_animation_thread()
+        self.stop_signal = False
+
+    def _new_animation_thread(self) -> Thread:
+        animation_thread = Thread(target=self._animate)
+        animation_thread.daemon = True
+        return animation_thread
+
+    def _animate(self):
+        if self.repeat_count is None:
+            while not self.stop_signal:
+                self._animation_iteration()
+
+        repeat_iterations: int = 0
+        while not self.stop_signal and repeat_iterations < self.repeat_count:
+            self._animation_iteration()
+            repeat_iterations += 1
+
+    def _animation_iteration(self):
+        self._animation_loop()
+        if self.auto_revert:
+            self._animation_loop(is_backward=True)
--- src/magique/animations/number_animation.py
+++ src/magique/animations/number_animation.py
@@ -1,5 +1,4 @@
 from typing import Callable, Any
-from threading import Thread, Lock
 from time import time, sleep
 
 from ..declarative import Observable
@@ -24,8 +23,6 @@ class NumberAnimation(Animation):
      - ``repeat_count`` - set up for repeat policy. In default = 1, if `None` the animation loop is endless
     """
 
-    lock = Lock()
-
     def __init__(
             self,
             target: Observable,
@@ -38,8 +35,7 @@ class NumberAnimation(Animation):
             auto_revert: bool = False,
             repeat_count: int | None = 1):
 
-        super().__init__(target, auto_revert, repeat_count)
-        self.animation_thread: Thread = self._new_animation_thread()
+        super().__init__(target, from_value, to_value, auto_revert, repeat_count)
 
         self.from_value: Number = from_value or target.value
         self.to_value: Number = to_value or target.value
@@ -47,96 +43,6 @@ class NumberAnimation(Animation):
         self.duration_sec: float = duration_sec
         self.fps: float = fps
         self.converter: Callable[[Any], Any] = converter
-        self.current_value: Number = self.from_value
-
-    def start(self, stop_running_animation: bool = False):
-        """
-        Enables the animation thread. If animation thread is enabled,
-        it can stop it and start new animation thread
-
-        :param stop_running_animation: If `True` the running thread will be stopped,
-        else the running thread stays running, new animation not starting
-        """
-
-        self.lock.acquire()
-        if self.is_active:
-            if not stop_running_animation:
-                if self.animation_thread.is_alive():
-                    self.lock.release()
-                    return
-
-            self.reset()
-
-        try:
-            self.is_active = True
-            self.animation_thread.start()
-
-        finally:
-            self.lock.release()
-
-    def pause(self):
-        """
-        Pauses the animation, with ability to resume when it's needed
-        """
-
-        self.is_active = False
-
-    def resume(self):
-        """
-        Resumes the paused animation
-        """
-
-        self.is_active = True
-
-    def reset(self):
-        """
-        Stops the animation thread and sets target value = start animation value (``from_value``)
-        """
-
-        self._stop_animation_thread()
-        self.target.value = self.from_value
-
-    def skip(self):
-        """
-        Stops the animation thread and sets target value = end animation value (``to_value``)
-        """
-
-        self._stop_animation_thread()
-        self.target.value = self.to_value
-
-    def wait_until_done(self):
-        """
-        Joins the animation thread (make you to wait until the animation done)
-        """
-
-        self.animation_thread.join()
-
-    def _stop_animation_thread(self):
-        self.is_active = False
-        self.stop_signal = True
-        self.animation_thread.join()
-        self.animation_thread = self._new_animation_thread()
-        self.stop_signal = False
-
-    def _new_animation_thread(self) -> Thread:
-        animation_thread = Thread(target=self._animate_number)
-        animation_thread.daemon = True
-        return animation_thread
-
-    def _animate_number(self):
-        if self.repeat_count is None:
-            while not self.stop_signal:
-                self._animation_iteration()
-
-        repeat_iterations: int = 0
-        while not self.stop_signal and repeat_iterations < self.repeat_count:
-            self._animation_iteration()
-            repeat_iterations += 1
-
-    def _animation_iteration(self):
-        self._animation_loop()
-        if self.auto_revert:
-            self._animation_loop(is_backward=True)
 
     def _animation_loop(self, is_backward: bool = False):
         frame_time_seconds: float = 1 / self.fps
@@ -163,8 +69,7 @@ class NumberAnimation(Animation):
                 if progress > 1.0: progress = 1.0
 
             eased_value: float = self.easing_function(progress)
-            self.current_value = self.from_value + self.converter(delta * eased_value)
-            self.target.value = self.current_value
+            self.target.value = self.from_value + self.converter(delta * eased_value)
 
             sleep(frame_time_seconds)
             reached_frames += 1
